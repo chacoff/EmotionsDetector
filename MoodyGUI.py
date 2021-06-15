2028acbf011c1286c5f0e379f4b8bdc63354c71b
chacoff <71355264+chacoff@users.noreply.github.com>
2021-06-16 00:44:42 +0200

    Add files via upload

--- MoodyGUI.py
+++ MoodyGUI.py
@@ -3,11 +3,11 @@ from tensorflow.keras.models import model_from_json
 import numpy as np
 import cv2
 from numpy import random, moveaxis
-import os
+import os, sys
 import pyautogui
 from win32api import GetSystemMetrics
+import xml.etree.ElementTree as ET
 from Emotions_UI_final import load_model, predict_emotion
-sg.theme('Reddit')
 
 
 def standbyVideo(f3, f4):
@@ -16,65 +16,72 @@ def standbyVideo(f3, f4):
     window['image'].update(data=imgbytes)
 
 
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
 if __name__ == '__main__':
 
+    # ---------- XML PARAMETERS SETTINGS ---------- #
+    tree = ET.parse(resource_path('params.xml'))
+    root = tree.getroot()
+    THEME = root[0][0].text
+    HALO = root[0][1].text
+    CAM = True if root[0][2].text == 'YES' else False  # if True, the webcam/video will be capture if False, the primary screen will be capture
+    VID = root[0][3].text  # 0 for webcam or video address, example 'vidk.mp4'
+    REC = True if root[0][4].text == 'YES' else False  # to record the webcam or the screen
+    REC_file = root[0][5].text
+    fps_xml = int(root[0][12].text)
+    FACTOR = float(root[0][6].text)  # factor to reduce the size of the screen in within the UI
+    SingleChannel = True  # currently only supporting 1 channel
+
+    # face detector
+    protoPath = os.path.join(root[0][7].text, root[0][8].text)  # face detector based on a res net
+    modelPath = os.path.join(root[0][7].text, root[0][9].text)  # face detector
+    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+    threshold = float(root[0][10].text)  # to filter out weak face detections
+    px_filter = 10  # size filter to ensure a sufficiently large face
+
+    # emotions classifier
+    emo_model = os.path.join(root[0][7].text, root[0][11].text)
+    model = load_model(emo_model)  # emotions classifier model
+    # emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+    emotion_dict = {0: "upset", 1: "happy", 2: "neutral"}
+
+    # color_cycle = [[random.randint(10, 255) for _ in range(3)] for _ in range(len(emotion_dict))]
+    color_cycle = [(30, 30, 250), (0, 210, 0), (0, 220, 220)]
+
+    # print(THEME, HALO, CAM, VID, REC, REC_file, FACTOR, protoPath, modelPath, threshold, px_filter, emo_model)
+    # ---------- XML PARAMETERS SETTINGS ---------- #
+
     # ---------- LAYOUT ---------- #
+    sg.theme(THEME)  # Reddit
     layout = [[sg.Image(filename='', key='image')],
-
-        # [sg.Text('Face Detector', size=(14, 1), auto_size_text=False, justification='left'),
-        #  sg.InputText('', size=(14, 1), enable_events=True, key="-FACE_MOD-"),
-        #  sg.FolderBrowse(size=(10, 1), font='Helvetica 8')],
-
-        # [sg.Text('Emotion\'s model', size=(14, 1), auto_size_text=False, justification='left'),
-        #  sg.InputText('', size=(14, 1), enable_events=True, key="-EMO_MOD-"),
-        # sg.FolderBrowse(size=(10, 1), font='Helvetica 8')],
 
         [sg.Button('Process', size=(10, 1), font='Helvetica 8'),
          sg.Button('Stop', size=(10, 1), font='Helvetica 8'),
          sg.Button('Exit', size=(10, 1), font='Helvetica 8'), ]]
 
     # create the window and show it without the plot
-    window = sg.Window('MoodyNet', layout, location=(800, 400))
+    window = sg.Window('MoodyNet', layout, location=(800, 400), alpha_channel=HALO)
     # ---------- LAYOUT ---------- #
-
-    # ---------- PARAMETERS SETTINGS ---------- #
-    CAM = False  # if True, the webcam/video will be capture if False, the primary screen will be capture
-    VID = 0  # 0 for webcam or video address, example 'vidk.mp4'
-    REC = False  # to record the webcam or the screen, must be True for single video processing
-    REC_file = 'jup06_4classes.avi'
-    FACTOR = 2.5  # factor to reduce the size of the screen in within the UI
-    SingleChannel = True  # currently only supporting 1 channel
-
-    # face detector
-    protoPath = os.path.join('models', 'deploy.prototxt')  # face detector based on a res net
-    modelPath = os.path.join('models', 'res10_300x300_ssd_iter_140000.caffemodel')  # face detector
-    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
-    threshold = 0.45  # to filter out week face detections
-    px_filter = 10  # size filter to ensure a sufficiently large face
-
-    # emotions classifier
-    path = "models/jup09/"
-    model = load_model(path)
-    # emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
-    emotion_dict = {0: "upset", 1: "happy", 2: "neutral"}
-
-    # color_cycle = [[random.randint(10, 255) for _ in range(3)] for _ in range(len(emotion_dict))]
-    color_cycle = [(30, 30, 250), (0, 210, 0), (0, 220, 220)]
-    # ---------- PARAMETERS SETTINGS ---------- #
 
     # -------- Event LOOP Read and display frames, operate the GUI -------- #
     if CAM:
+        VID = int(VID) if VID == '0' else VID
         webcam = cv2.VideoCapture(VID)
         f3 = int(webcam.get(3))  # width
         f4 = int(webcam.get(4))  # height
         recording = False
     else:
-        f3 = int(GetSystemMetrics(0) / FACTOR)  # width
-        f4 = int(GetSystemMetrics(1) / FACTOR)  # height
+        f3 = int(GetSystemMetrics(0) / float(FACTOR))  # width
+        f4 = int(GetSystemMetrics(1) / float(FACTOR))  # height
         recording = False
 
     if REC is True:
-        out = cv2.VideoWriter(REC_file, cv2.VideoWriter_fourcc(*'XVID'), 12, (f3, f4))
+        out = cv2.VideoWriter(REC_file, cv2.VideoWriter_fourcc(*'XVID'), fps_xml, (f3, f4))
 
     while True:
         event, values = window.read(timeout=20)  # pysimpleGUI
